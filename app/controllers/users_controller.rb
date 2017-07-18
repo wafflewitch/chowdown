4dbce0395fae63ba9d2a4a8c3899de416dc98d19
Sophie Meier <mesophie@gmx.ch>
2017-07-18 11:57:54 -0400

    added user-controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/show'
+
+  get 'users/create'
+
+  get 'users/destroy'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
