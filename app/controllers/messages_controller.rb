a494e84838683ebd2971a1127d34d9f8c580a316
Joe Van Kirk <imjoevankirk@gmail.com>
2017-07-19 13:15:47 -0400

    added logic to index page in views/messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,30 @@
 class MessagesController < ApplicationController
+  before_action do
+    @chow = Chow.find(params[:chow_id])
+  end
+
+  def index
+    # show all messages
+    @messages = @chow.messages.all
+  end
+
+  def new
+    # create new message
+    @message = @chow.messages.new
+  end
+
+  def create
+    @message = @chow.messages.new(message_params)
+    if @message.save
+      redirect_to user_chow_messages_path(@chow)
+    end
+  end
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :sender_id, :recipient_id)
+  end
+
 end
