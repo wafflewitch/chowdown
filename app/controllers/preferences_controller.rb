5091f1208de91a98f83d95eb9e9a145ba9777f38
Jessica Hines <jessicahines717@gmail.com>
2017-07-19 13:13:53 -0400

    started on prefs controller

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,2 +1,24 @@
 class PreferencesController < ApplicationController
+  before_action :set_user
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user[:id])
+  end
 end
