0cfc557f8d7c3cee6f7ecc4899dea9d3a768f437
Jessica Hines <jessicahines717@gmail.com>
2017-07-19 14:01:22 -0400

    wrote basic controllers for badges and prefs

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,24 +1,52 @@
 class PreferencesController < ApplicationController
   before_action :set_user
+  before_action :set_pref, only: [ :show, :edit, :update, :destroy ]
 
   def new
+    @pref = DietPref.new
   end
 
   def create
+    @pref = DietPref.new(pref_params)
+    @pref.user = @user
+    if @pref.save!
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @prefs = DietPref.where(users_id: @user.id)
+  end
+
+  def show
   end
 
   def edit
   end
 
   def update
+    @pref.update(pref_params)
+    redirect_to user_path(@user)
   end
 
   def destroy
+    @pref.destroy
+    redirect_to user_path(@user)
   end
 
   private
 
   def set_user
     @user = User.find(current_user[:id])
+  end
+
+  def set_pref
+    @pref = DietPref.find(params[:id])
+  end
+
+  def pref_params
+    params.require(:diet_pref).permit(:users_id, :name, :icon)
   end
 end
