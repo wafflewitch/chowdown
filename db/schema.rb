ff6d2b8ff4fb882d2f2102fb219829e3c6b3589b
Jessica Hines <jessicahines717@gmail.com>
2017-07-20 17:07:47 -0400

    added datepicker for calendar new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720162843) do
+ActiveRecord::Schema.define(version: 20170720200853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170720162843) do
     t.date     "dates",      default: [],              array: true
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.integer  "chow_id"
     t.index ["user_id"], name: "index_calendars_on_user_id", using: :btree
   end
 
