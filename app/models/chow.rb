6533a97e8ab6769704dd7aa707f958d348ac9721
Jessica Hines <jessicahines717@gmail.com>
2017-07-21 17:20:30 -0400

    commenting out mailer stuff for now

--- app/models/chow.rb
+++ app/models/chow.rb
@@ -4,24 +4,24 @@ class Chow < ApplicationRecord
   has_one :calendar1, :class_name => "Calendar", :foreign_key =>"calendar_1_id"
   has_one :calendar2, :class_name => "Calendar", :foreign_key =>"calendar_2_id"
   has_many :messages
-  after_create :send_request_email
+  # after_create :send_request_email
   # after_update :send_status_accepted
   # after_update :send_status_rejected
 
 
-  def send_request_email
-    ChowMailer.chowdown_request(self).deliver_now
-    self.status = "pending"
-    self.save!
-  end
+  # def send_request_email
+  #   ChowMailer.chowdown_request(self).deliver_now
+  #   self.status = "pending"
+  #   self.save!
+  # end
 
-  def send_status_accepted
-    ChowMailer.chowdown_accepted(self).deliver_now
-  end
+  # def send_status_accepted
+  #   ChowMailer.chowdown_accepted(self).deliver_now
+  # end
 
-  def send_status_rejected
-    ChowMailer.chowdown_rejected(self).deliver_now
-  end
+  # def send_status_rejected
+  #   ChowMailer.chowdown_rejected(self).deliver_now
+  # end
 
 
 end
