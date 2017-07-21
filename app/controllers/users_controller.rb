9912d38e7c790e50fc7c7a46e7e1c60dd3c21870
Jessica Hines <jessicahines717@gmail.com>
2017-07-21 16:24:58 -0400

    took out empty method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [ :set_up_profile, :show, :edit, :update, :destroy ]
+  before_action :set_user, only: [ :show, :edit, :update, :destroy ]
 
   def new
   end
