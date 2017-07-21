f74f15a2821404391cb44d4fce865b363a4bc1c3
Jessica Hines <jessicahines717@gmail.com>
2017-07-21 15:15:21 -0400

    changed root and plugged bio into users#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  root to: 'pages#home'
+  root to: 'users#index'
 
   resources :users do
     resources :chows, only: [ :index, :new, :create, :show, :destroy, :update] do
