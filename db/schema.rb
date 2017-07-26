6cbfa32fa1234b41eed65c0f966464533cde79ec
Sophie Meier <mesophie@gmx.ch>
2017-07-26 11:42:57 -0400

    remove migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725211601) do
+ActiveRecord::Schema.define(version: 20170726140338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,17 @@ ActiveRecord::Schema.define(version: 20170725211601) do
     t.index ["chow_id"], name: "index_messages_on_chow_id", using: :btree
   end
 
+  create_table "recipes", force: :cascade do |t|
+    t.boolean  "user1"
+    t.boolean  "user2"
+    t.string   "title"
+    t.string   "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",   null: false
     t.string   "encrypted_password",     default: "",   null: false
@@ -117,4 +128,5 @@ ActiveRecord::Schema.define(version: 20170725211601) do
   add_foreign_key "decisions", "users", column: "user_1_id"
   add_foreign_key "decisions", "users", column: "user_2_id"
   add_foreign_key "messages", "chows"
+  add_foreign_key "recipes", "users"
 end
