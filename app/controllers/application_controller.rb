6b0ca1bd3c8d5a619e67b287b6f845ef6def1b14
Sophie Meier <mesophie@gmx.ch>
2017-07-26 19:41:16 -0400

    editet meta data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,4 +33,10 @@ class ApplicationController < ActionController::Base
       img = "https://res.cloudinary.com/wafflewitch/image/upload/v1500649828/wheat_no.png"
     end
   end
+
+
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
 end
