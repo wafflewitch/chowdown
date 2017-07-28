05cc980c29451c1a7a36ae125f0a59b4065233ee
Louis de La Marnierre <empsalle@gmail.com>
2017-07-28 16:21:54 -0400

    seed and like

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     Decision.where(user2: current_user.id).collect(&:user_1_id).each do |id|
       ids << id
     end
-
+    ids << current_user.id
     if params[:matchingPref] == 'true'
       @users = User.near([current_user.latitude.to_f, current_user.longitude.to_f], params[:maxDistance], units: :km, :order => 'distance')
                     .where(dating: params[:datingPref])
